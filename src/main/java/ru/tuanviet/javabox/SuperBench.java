bcf51793b44fffd11250bad35acddbbb83a3bbe6
AndrewRosso <a.krasnov91@mail.ru>
2021-11-23 23:05:31 +0300

    add isolated methods

--- src/main/java/ru/tuanviet/javabox/SuperBench.java
+++ src/main/java/ru/tuanviet/javabox/SuperBench.java
@@ -9,9 +9,7 @@ import java.util.*;
 
 public class SuperBench {
     public void benchmark(Class<?>[] classes) {
-        System.out.println("-- OUTPUT ------------------------------------------------");
-        System.out.println("Benchmark started at " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm:ss.SSS")));
-        System.out.println("");
+        printHeading();
 
         for (Class oneOfClasses : classes) {
             try {
@@ -21,66 +19,60 @@ public class SuperBench {
 
                 Annotation annotations = method.getAnnotation(Benchmark.class);
                 Benchmark mBenchmark = (Benchmark) annotations;
-//                System.out.println(mBenchmark.repeats());
-//                System.out.println(mBenchmark.timeout());
-
-                long startTime = 0;
-                long endTime = 0;
-                long minimal = 0;
-                long average = 0;
-                long sumOfResultTime = 0;
-                long maximum = 0;
-                int successfulRepeat = 0;
-                String resultTest = "FAILED";
+                int repeats = mBenchmark.repeats();
+                long timeout = mBenchmark.timeout();
 
                 List<Long> resultTime = new ArrayList<>();
 
-                for (int i = 0; i < mBenchmark.repeats(); i++) {
+                for (int i = 0; i < repeats; i++) {
                     try {
-                        startTime = System.nanoTime();
+                        long startTime = System.nanoTime();
                         method.invoke(instance);
-                        endTime = System.nanoTime();
-                        successfulRepeat++;
+                        long endTime = System.nanoTime();
                         resultTime.add(endTime - startTime);
-                        sumOfResultTime += resultTime.get(i);
-                        if (resultTime.get(i) > mBenchmark.timeout()) {
+                        if (resultTime.get(i) > timeout) {
                             break;
                         }
-//                            System.out.println(endTime - startTime);
-                    } catch (IllegalAccessException e) {
-                        e.printStackTrace();
-                    } catch (InvocationTargetException e) {
+                    } catch (IllegalAccessException | InvocationTargetException e) {
                         e.printStackTrace();
                     }
                 }
 
-                minimal = Collections.min(resultTime);
-                average = sumOfResultTime/successfulRepeat;
-                maximum = Collections.max(resultTime);
+                printMethodsTimeStats(methodName, resultTime, repeats, timeout);
 
-                if (successfulRepeat == mBenchmark.repeats()) resultTest = "PASSED";
-
-                System.out.println("[Test " + UUID.randomUUID() + " " + resultTest + "]");
-                System.out.println(methodNameFormatter(methodName));
-                System.out.println("Repeats: " + successfulRepeat + "/" + mBenchmark.repeats());
-                System.out.println("Timeout: " + mBenchmark.timeout());
-                System.out.println("Min: "+minimal);
-                System.out.println("Avg: "+average);
-                System.out.println("Max: "+maximum);
-
-                System.out.println("75% of the numbers are less than or equal to "+getPercentile(resultTime,75));
-                System.out.println("95% of the numbers are less than or equal to "+getPercentile(resultTime,95));
-                System.out.println("99% of the numbers are less than or equal to "+getPercentile(resultTime,99));
-
-                System.out.println("");
-
-
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            } catch (InstantiationException e) {
+            } catch (IllegalAccessException | InstantiationException e) {
                 e.printStackTrace();
             }
         }
+    }
+
+    private void printHeading() {
+        System.out.println("-- OUTPUT ------------------------------------------------");
+        System.out.println("Benchmark started at "
+                + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm:ss.SSS")));
+        System.out.println("");
+    }
+
+    private void printMethodsTimeStats(String methodName, List<Long> resultTime, int repeat, long timeout) {
+        String resultTest = "FAILED";
+        int successfulRepeat = resultTime.size();
+
+        if (successfulRepeat == repeat) resultTest = "PASSED";
+
+        System.out.println("[Test " + UUID.randomUUID() + " " + resultTest + "]");
+        System.out.println(methodNameFormatter(methodName));
+        System.out.println("Repeats: " + successfulRepeat + "/" + repeat);
+        System.out.println("Timeout: " + timeout);
+
+        System.out.println("Min: " + Collections.min(resultTime));
+        System.out.println("Avg: " + average(resultTime));
+        System.out.println("Max: " + Collections.max(resultTime));
+
+
+        System.out.println("75% of the numbers are less than or equal to " + getPercentile(resultTime, 75));
+        System.out.println("95% of the numbers are less than or equal to " + getPercentile(resultTime, 95));
+        System.out.println("99% of the numbers are less than or equal to " + getPercentile(resultTime, 99));
+        System.out.println("");
     }
 
     private String methodNameFormatter(String methodName) {
@@ -93,17 +85,21 @@ public class SuperBench {
         } else {
             result = str.replaceAll("(?<=[A-Za-z])(?=[0-9])|(?<=[0-9])(?=[A-Za-z])|(?<=[a-z])(?=[A-Z])", " ");
         }
-
-        return "> "+result;
+        return "> " + result;
     }
-    private long getPercentile(List<Long> result, double percentile){
+
+    private long getPercentile(List<Long> result, double percentile) {
         Collections.sort(result);
-        int index = (int)Math.ceil(((double)percentile / (double)100) * (double)result.size());
-        return result.get(index-1);
-    }
-    private void printMethodsTimeStats (String methodName,List<Long> resultTime,int repeat,long timeout){
-
+        int index = (int) Math.ceil((percentile / (double) 100) * (double) result.size());
+        return result.get(index - 1);
     }
 
+    private long average(List<Long> resultTime) {
+        long sumOfResultTime = 0;
+        for (long time : resultTime) {
+            sumOfResultTime += time;
+        }
+        return sumOfResultTime / resultTime.size();
+    }
 }
 
